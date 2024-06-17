4883f8906acede47f3d4c3a0f68cd236a3ee2a0c
Avaab Razzaq <40202832+AR10Dev@users.noreply.github.com>
2024-06-17 17:59:11 +0000

    chore: Add API endpoint for serving embedded files and implement video streaming

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,38 +1,22 @@
-use actix_web::{get, middleware::{Compress, Logger}, web, App, HttpResponse, HttpServer, Responder};
-use rust_embed::RustEmbed;
-use std::borrow::Cow;
+mod api;
+mod embed;
+mod streaming;
 
-#[derive(RustEmbed)]
-#[folder = "dist/"]
-struct Asset;
-
-async fn index() -> impl Responder {
-    let index = web::Path::from("index.html".to_string());
-    serve_file(index).await
-    // HttpResponse::Ok().content_type("text/html").body(include_str!("../dist/index.html"))
-}
-
-async fn serve_file(path: web::Path<String>) -> impl Responder {
-    let file_path = path.into_inner();
-    match Asset::get(&file_path) {
-        Some(content) => HttpResponse::Ok()
-            .content_type(
-                mime_guess::from_path(&file_path)
-                    .first_or_octet_stream()
-                    .as_ref(),
-            )
-            .body(Cow::into_owned(content.data)),
-        None => HttpResponse::NotFound().body("404 Not Found"),
-    }
-}
-
-#[get("/hello")]
-async fn hello() -> &'static str {
-    "Hello, world!"
-}
+use actix_cors::Cors;
+use actix_web::{
+    middleware::{Compress, Logger},
+    App, HttpServer,
+};
+use api::hello;
+use embed::{dist, index};
+use std::env;
+use streaming::video_stream;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug");
+    env_logger::init();
+
     let host = "0.0.0.0";
     let port = 9080;
 
@@ -40,9 +24,11 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(Logger::default())
             .wrap(Compress::default())
-            .route("/", web::get().to(index))
+            // .wrap(Cors::default())
+            .service(index)
             .service(hello)
-            .route("/{filename:.*}", web::get().to(serve_file))
+            .service(video_stream)
+            .service(dist)
     })
     .bind((host, port))?
     .run()
